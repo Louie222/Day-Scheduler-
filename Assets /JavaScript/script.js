f51ab5c4a5ce1e043f214b867332fdbaf4a2c636
Louie Obaldo <louie.obaldo@education.wa.edu.au>
2021-04-11 12:17:13 +0800

    Updating my Assignment 5

---
+++ Assets /JavaScript/script.js
@@ -0,0 +1,27 @@
+//GIVEN I am using a daily planner to create a schedule
+//WHEN I open the planner
+//THEN the current day is displayed at the top of the calendar
+//WHEN I scroll down
+//THEN I am presented with timeblocks for standard business hours
+//WHEN I view the timeblocks for that day
+//THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+//WHEN I click into a timeblock
+//THEN I can enter an event
+//WHEN I click the save button for that timeblock
+//THEN the text for that event is saved in local storage
+//WHEN I refresh the page
+//THEN the saved events persist
+
+const nineAM = document.getElementById('9am');
+const tenAM = document.getElementById('10am');
+const elevenAM = document.getElementById('11am');
+const twelvePM = document.getElementById('12pm');
+const onePM = document.getElementById('1pm');
+const twoPM = document.getElementById('2pm');
+const threePM = document.getElementById('3pm');
+const fourPM = document.getElementById('4pm');
+const fivePM = document.getElementById('5pm');
+
+
+
+
